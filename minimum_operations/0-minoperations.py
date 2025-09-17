e67bed03a8b7918bf0481da199d318419fa7fb8d
Ali-Rashid85 <ali.n@academicbridge.xyz>
2025-09-17 17:20:02 +0200

    fix formatting in minOperations function for improved readability

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,10 +3,11 @@
 """Calculates the minimum number of operations to reach n 'A's starting from one 'A'. With only two operations
 allowed: Copy All and Paste."""
 
+
 def minOperations(n):
     ops = 0
     while n > 1:
-        for i in range(2, n+1):
+        for i in range(2, n + 1):
             if n % i == 0:
                 n = n // i
                 ops += i
