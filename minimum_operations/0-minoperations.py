cb5f957c85af4848c9ed52b5574ef4d5f2817aeb
Ali-Rashid85 <ali.n@academicbridge.xyz>
2025-09-17 17:13:39 +0200

    add __init__.py file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""Calculates the minimum number of operations to reach n 'A's starting from one 'A'. With only two operations
+allowed: Copy All and Paste."""
+
 def minOperations(n):
-    """Calculates the minimum number of operations to reach n 'A's starting from one 'A'. With only two operations allowed: Copy All and Paste."""
     ops = 0
     while n > 1:
         for i in range(2, n+1):
@@ -10,7 +12,3 @@ def minOperations(n):
                 ops += i
                 break
     return ops
-
-
-if __name__ == "__main__":
-    print(minOperations(12))
